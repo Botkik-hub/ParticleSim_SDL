5d871a5d9a731524f717303632c122c7756e3ece
Stanislav Voronov <stasv2003@gmail.com>
2024-02-12 17:43:39 -0500

    looks not bad for now

--- ParticleSim/src/Filed.cpp
+++ ParticleSim/src/Filed.cpp
@@ -1,82 +1,130 @@
 ﻿#include "Filed.h"
 
+#include <algorithm>
+
 #include "TheGame.h"
 
 Filed::Filed()
 {
-    m_particles = new ParticleType*[TheGame::WIDTH];
-    for (int i = 0; i < TheGame::WIDTH; ++i)
-    {
-        m_particles[i] = new ParticleType[TheGame::HEIGHT];
-        for (int j = 0; j < TheGame::HEIGHT; ++j)
-        {
-            m_particles[i][j] = ParticleType::None;
-        }
-    }
+    m_width = TheGame::WIDTH;
+    m_height = TheGame::HEIGHT;
+    m_size = m_height * m_width;
+    
+    m_particles = new ParticleType[m_size];
+    //m_particlesBuffer = new Data[m_size];
     m_renderer = TheGame::Instance().GetRenderer();
 
-    for (int x = 100; x < 200; ++x)
+    for (int i = 0; i < m_size; ++i)
+    {
+        m_particles[i] = ParticleType::None;
+    }
+    // m_particles[Ind(300 ,300)] = ParticleType::Sand;
+    
+    for (int x = 170; x < 200; ++x)
     {
         for (int y = 50; y < 100; ++y)
         {
-            m_particles[x][y] = ParticleType::Sand;
+            m_particles[Ind(x,y)] = ParticleType::Sand;
         }
     }
-
-    for (int x = 200; x < TheGame::WIDTH; ++x)
+    
+    for (int x = 200; x < m_width; ++x)
     {
         for (int y = 50; y < 100; ++y)
         {
-            m_particles[x][y] = ParticleType::Water;
+            m_particles[Ind(x,y)] = ParticleType::Water;
         }
     }
 }
 
 Filed::~Filed()
 {
-    for (int i = 0; i < TheGame::WIDTH; ++i)
-    {
-        delete m_particles[i];
-    }
     delete m_particles;
 }
 
-void Filed::Update(float dt)
+void Filed::ApplyGravity(int x, int y) const
 {
-    for (int x = TheGame::WIDTH - 1; x >= 0; --x)
+    if (y + 1 == m_height)
     {
-        for (int y = TheGame::HEIGHT - 2; y >= 0; --y)
+        return;
+    }
+
+    const int current = Ind(x, y);
+    const int bottom = Ind(x, y + 1);
+    const int bottomRight = Ind(x + 1, y +1);
+    const int bottomLeft = Ind(x - 1, y +1);
+    
+    if (CanSwap(current, bottom))
+    {
+        // m_particles[bottom] = m_particles[Ind(x,y)];
+        // m_particles[current] = ParticleType::None;
+        SwapParticles(current,bottom);
+    }
+    else if (x - 1 > 0 && CanSwap(current, bottomLeft))
+    //else if (x - 1 > 0 && m_particles[bottomLeft] == ParticleType::None)
+    {
+        // m_particles[bottomLeft] = m_particles[current];
+        // m_particles[current] = ParticleType::None;
+        SwapParticles(current,bottomLeft);
+    }
+    else if (x + 1 < m_width && CanSwap(current, bottomRight))
+    // else if (x + 1 < m_width && m_particles[bottomRight] == ParticleType::None)
+    {
+        SwapParticles(current, bottomRight);
+        // m_particles[bottomRight] = m_particles[current];
+        // m_particles[current] = ParticleType::None;
+    }
+    else
+    {
+        //m_particles[Ind(x,y)]= m_particles[Ind(x,y)];
+    }
+}
+
+void Filed::UpdateTile(int x, int y) const
+{
+    ApplyGravity(x, y);
+}
+
+void Filed::Update(float dt) const
+{
+    for (int x = m_width - 1; x >= 0; --x)
+    {
+        for (int y = m_height - 1; y >= 0; --y)
         {
-            if (m_particles[x][y] != ParticleType::None)
+            if (m_particles[Ind(x,y)] != ParticleType::None)
             {
-                if (m_particles[x][y + 1] == ParticleType::None)
-                {
-                    m_particles[x][y + 1] = m_particles[x][y];
-                    m_particles[x][y] = ParticleType::None;
-                }
-                else if (x - 1 > 0 && m_particles[x - 1][y + 1] == ParticleType::None)
-                {
-                    m_particles[x-1][y+1] = m_particles[x][y];
-                    m_particles[x][y] = ParticleType::None;
-                }
-                else if (x + 1 < TheGame::WIDTH && m_particles[x + 1][y + 1] == ParticleType::None)
-                {
-                    m_particles[x+1][y+1] = m_particles[x][y];
-                    m_particles[x][y] = ParticleType::None;
-                }
+                UpdateTile(x, y);
             }
         }
     }
+
+    // Data* end = m_particlesBuffer + (m_width * m_height + 1);
+    // std::copy(m_particlesBuffer, end, m_particles);
+    // for (int x = 0; x < m_width; ++x)
+    // {
+    //     for (int y = 0; y < m_height; ++y)
+    //     {
+    //         const int i = Ind(x,y);
+    //         m_particles[i] = m_particlesBuffer[i];
+    //         m_particlesBuffer[i] = ParticleType::None;
+    //     }
+    // }
 }
 
 void Filed::Render() const
 {
-    for (int x = 0; x < TheGame::WIDTH; ++x)
+    ParticleType lastParticle = ParticleType::None;
+    for (int x = 0; x < m_width; ++x)
     {
-        for (int y = 0; y < TheGame::HEIGHT; ++y)
+        for (int y = 0; y < m_height; ++y)
         {
-            if (m_particles[x][y] == ParticleType::None) continue;
-            SetColor(m_particles[x][y]);
+            const ParticleType current = m_particles[Ind(x,y)];
+            if (current == ParticleType::None) continue;
+            if (lastParticle != current)
+            {
+                SetColor(current);
+                lastParticle = current;
+            }
             SDL_RenderDrawPoint(m_renderer, x, y);
         }
     }
@@ -85,7 +133,7 @@ void Filed::Render() const
 void Filed::SetColor(const ParticleType type) const
 {
     SDL_Color color;
-    switch (type)
+    switch (type)  // NOLINT(clang-diagnostic-switch-enum)
     {
         case ParticleType::Sand:
             color = {255, 255, 0, 255};
@@ -100,7 +148,37 @@ void Filed::SetColor(const ParticleType type) const
     SDL_SetRenderDrawColor(m_renderer, color.r, color.g, color.b, color.a);
 }
 
-void Filed::SwapParticles(int x1, int y1, int x2, int y2)
+void Filed::SwapParticles(const int x1, const int y1, const int x2, const int y2) const
 {
+    const int i1 = Ind(x1,y1);
+    const int i2 = Ind(x2,y2);
+    const ParticleType temp = m_particles[i1];
     
+    m_particles[i1] = m_particles[i2];
+    m_particles[i2] = temp;
 }
+
+void Filed::SwapParticles(const int i1, const int i2) const
+{
+    const ParticleType temp = m_particles[i1];
+    
+    m_particles[i1] = m_particles[i2];
+    m_particles[i2] = temp;
+}
+
+bool Filed::CanSwap(int i1, int i2) const
+{
+    if (m_particles[i2] == ParticleType::None) return true;
+    //if (m_particles[i1] == ParticleType::None) return true;
+    
+    if (m_particles[i1] == ParticleType::Sand && m_particles[i2] == ParticleType::Water) return true;
+    //if (m_particles[i2] == ParticleType::Sand && m_particles[i1] == ParticleType::Water) return true;
+
+    return false;
+}
+
+int Filed::Ind(const int x, const int y) const
+{
+    return x + y * m_width;
+}
+
--- ParticleSim/src/Filed.h
+++ ParticleSim/src/Filed.h
@@ -9,22 +9,39 @@ enum class ParticleType
     Water
 };
 
+struct Data
+{
+    ParticleType type = ParticleType::None;
+    bool hasMoved;
+};
 
 class Filed
 {
-    ParticleType** m_particles = nullptr;
-    //ParticleType** m_particlesBuffer = nullptr;
+    ParticleType* m_particles = nullptr;
+    //Data* m_particlesBuffer = nullptr;
     SDL_Renderer* m_renderer = nullptr;
+
+    int m_width;
+    int m_height;
+    int m_size;
 public:
 
     Filed();
     ~Filed();
-    
-    void Update(float dt);
+    void ApplyGravity(int x, int y) const;
+    void UpdateTile(int x, int y) const;
+
+    void Update(float dt) const;
     void Render() const;
 
 private:
     void SetColor(ParticleType type) const;
 
-    void SwapParticles(int x1, int y1, int x2, int y2);
+    void SwapParticles(int x1, int y1, int x2, int y2) const;
+
+    void SwapParticles(int i1, int i2) const;
+    
+    bool CanSwap(int i1, int i2) const;
+    
+    int Ind(int x, int y) const;
 };
